2310a6968c26ed27735a97be26704c75eb6afff5
Joane <138986247+japcunha@users.noreply.github.com>
2025-04-11 10:19:04 -0300

    add middlewars

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,11 +1,30 @@
 const tasksModel = require('../models/tasksModels');
+
 const getAll = async (re, res) => {
    const tasks = await tasksModel.getAll();
 
    return res.status(200).json(taks);
 };
-
+const createTask = async (request, response) => {
+   const createdTask = await tasksModel.createTask(request.body);
+   return response.status(201).json(createdTask);
+ };
+ const deleteTask = async (request, response) => {
+   const { id } = request.params;
+ 
+   await tasksModel.deleteTask(id);
+   return response.status(204).json();
+ }; 
+ const updateTask = async (request, response) => {
+   const { id } = request.params;
+ 
+   await tasksModel.updateTask(id, request.body);
+   return response.status(204).json();
+ };
  module.exports = {
-    getAll
+    getAll,
+    createTask,
+    deleteTask,
+    updateTask,
  };
  
